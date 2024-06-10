80318cc59aabf0706bbc0a1634300adf092be9d9
Kevin Guedes <kevinguedes1@gmail.com>
2024-06-10 09:52:47 -0300

    fix: with flushsync

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,7 @@
 import { createFileRoute, useRouter } from '@tanstack/react-router'
 import { useAtom } from 'jotai'
 import { userAtom } from '../stores/user-store'
+import { flushSync } from 'react-dom'
 
 export const Route = createFileRoute('/')({
   component: Index
@@ -10,20 +11,25 @@ function Index() {
   const [user, setUser] = useAtom(userAtom)
   const router = useRouter()
 
-
   async function login() {
     await new Promise((resolve) => {
       setTimeout(() => {
+        flushSync(() => {
         setUser({name: 'Guest', isAuthenticated: true})
+
+        })
         resolve(1)
-      }, 10)
+      }, 100)
     })
   }
 
   async function handleLogin() {
     await login()
     await router.invalidate()
+
+    // await new Promise((resolve) => setTimeout(resolve, 10))
     await router.navigate({to: '/about'})
+  
   }
 
   return <div>
